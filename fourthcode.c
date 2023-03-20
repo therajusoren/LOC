5c6af6653740dddff18afcd64f895823336ec3fa
Raju Soren <124340897+therajusoren@users.noreply.github.com>
2023-03-20 08:11:44 +0530

    First week assignment

--- fourthcode.c
+++ fourthcode.c
@@ -1,35 +1,34 @@
 #include<stdio.h>
 void main()
 {
-    printf("Enter 1st Number : ");
-    int num1;
+    int num1, num2, num3, ans; 
+    float num4;
+    printf("Enter first number : ");
     scanf("%d",&num1);
-    printf("Enter 2nd Number : ");
-    int num2;
+    printf("Enter second number : ");
     scanf("%d",&num2);
+    printf(" For summation type (1)\nFor subtraction type (2) \nFor multiplication type (3)\nFor division type (4) \n");
     printf("Which operation you want to do : ");
-    int ans;
     scanf("%d",&ans);
     switch(ans)
     {
-        case 1 :
+        case 1:
         int sum = num1 + num2;
-        printf("Sum of these numbers %d", sum);
+        printf("Sum of these numbers :%d",sum);
         break;
-        case 2 :
-        int sub = num1 - num2;
-        printf("Substraction of these numbers %d", sub);
+        case 2:
+        int dif = num1 - num2;
+        printf("Substraction of these numbers : %d",dif);
         break;
-        case 3 :
-        int mul = num1 * num2 ;
-        printf("Multiplication of these numbers %d", mul);
+        case 3:
+        int mul = num1 * num2;
+        printf("Multiplication of these numbers : %d",mul);
         break;
         case 4:
-        int div = num1 / num2 ;
-        printf("Division of these to numbers %d", div);
+        float div = num1 / num2;
+        printf("Division of these numbers :%f",div);
         break;
     }
-
-}
-
+    printf("\nThank you");
+    return 0;
 }
